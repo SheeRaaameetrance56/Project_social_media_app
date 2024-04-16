5126e1c74d996151952ccaae8f9a4c0646abc624
Shehan wijeratne <shehanranjukaw5@gmail.com>
2024-04-16 23:29:28 +0530

    Create authentication form component

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,14 +1,20 @@
-import { Box, Container, Flex } from '@chakra-ui/react'
+import { Box, Container, Flex, Image, VStack } from '@chakra-ui/react'
 import React from 'react'
 
 export default function AuthPage() {
   return (
     <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
         <Container maxW={"container.md"} padding={0}>
+
             {/* left hand side */}
             <Box display={{base:"none", md:"block"}}>
-
+                <Image src='/auth.png' h={650} alt='Phone img'/>
             </Box>
+
+            {/* right hand side */}
+            <VStack spacing={4} align={"stretch"}>
+                <AuthForm/>
+            </VStack>
         </Container>
     </Flex>
   )
