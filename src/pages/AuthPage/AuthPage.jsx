39a918302d7ad83d71324650a94bf941fe1eb51e
Shehan wijeratne <shehanranjukaw5@gmail.com>
2024-04-16 23:22:50 +0530

    remake public folder for assets

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,7 +1,15 @@
+import { Box, Container, Flex } from '@chakra-ui/react'
 import React from 'react'
 
 export default function AuthPage() {
   return (
-    <div>AuthPage</div>
+    <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
+        <Container maxW={"container.md"} padding={0}>
+            {/* left hand side */}
+            <Box display={{base:"none", md:"block"}}>
+
+            </Box>
+        </Container>
+    </Flex>
   )
 }
