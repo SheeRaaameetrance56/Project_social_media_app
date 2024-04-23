639ae8833949ea963cb66ebe36759420ecb5706c
Shehan wijeratne <shehanranjukaw5@gmail.com>
2024-04-23 21:34:11 +0530

    structure suggested users

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import SuggestedUser from './SuggestedUser'
+import { VStack } from '@chakra-ui/react'
+import SuggestedHeader from './SuggestedHeader'
 
 function SuggestedUsers() {
   return (
-    <>
+    <VStack py={8} px={6} gap={4}>
+        <SuggestedHeader/>
         <SuggestedUser/>
         <SuggestedUser/>
         <SuggestedUser/>
         <SuggestedUser/>
-    </>
+    </VStack>
   )
 }
 
