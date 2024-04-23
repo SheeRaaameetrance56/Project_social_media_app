085d241951281444e8d46eecd6db44fb65b57db2
Shehan wijeratne <shehanranjukaw5@gmail.com>
2024-04-23 17:19:11 +0530

    add post component

--- src/components/Feedposts/Feedposts.jsx
+++ src/components/Feedposts/Feedposts.jsx
@@ -1,8 +1,16 @@
+import { Container } from '@chakra-ui/react'
 import React from 'react'
+import FeedPost from './FeedPost'
 
 function Feedposts() {
   return (
-    <div>Feedposts</div>
+    <Container maxW={"container.sm"} py={10} px={2}>
+        <FeedPost/>
+        <FeedPost/>
+        <FeedPost/>
+        <FeedPost/>
+        <FeedPost/>
+    </Container>
   )
 }
 
