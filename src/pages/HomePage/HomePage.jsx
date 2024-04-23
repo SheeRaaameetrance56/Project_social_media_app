54051a8c8a319c588096ffe911782d5040826fe2
Shehan wijeratne <shehanranjukaw5@gmail.com>
2024-04-23 17:11:04 +0530

    structure home page for posts and suggestions

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,17 @@
+import { Box, Container, Flex } from '@chakra-ui/react'
 import React from 'react'
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <Container maxW={"container.lg"}>
+      <Flex gap={20}>
+        <Box flex={2} py={10} border={"1px solid blue"}>
+          <Feedpost/>
+        </Box>
+        <Box flex={3} mr={20} display={{base:"none", lg:"block"}} maxW={"300px"} border={"1px solid red"}>
+          Suggested
+        </Box>
+      </Flex>
+    </Container>
   )
 }
