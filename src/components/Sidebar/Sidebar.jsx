9c8461ed99dfed53c84eaf3d6a17dde380f70b69
Shehan wijeratne <shehanranjukaw5@gmail.com>
2024-06-04 19:27:55 +0530

    logout bug fixes

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -10,7 +10,7 @@ import useLogOut from '../../hooks/useLogOut';
 
 function Sidebar() {
 
-    const {handleLogout, isLoggingOut, error} = useLogOut();
+    const {handleLogOut, isLoggingOut} = useLogOut();
 
     const sidebarItems = [
         {
@@ -74,7 +74,7 @@ function Sidebar() {
             </Flex>
             {/* Log out */}
             <Tooltip hasArrow label={"Log out"} placement='right' ml={1} openDelay={500} display={{base:'block', md:'none'}}>
-                <Flex onClick={handleLogout} alignItems={'center'} gap={4} _hover={{bg:"whiteAlpha.400"}} borderRadius={6} p={2} w={{base:10, md:'full'}} mt={'auto'} justifyContent={{base:'center', md:'flex-start'}} cursor={'pointer'}>
+                <Flex onClick={handleLogOut} alignItems={'center'} gap={4} _hover={{bg:"whiteAlpha.400"}} borderRadius={6} p={2} w={{base:10, md:'full'}} mt={'auto'} justifyContent={{base:'center', md:'flex-start'}} cursor={'pointer'}>
                     <BiLogOut size={25}/>
                     <Button display={{base:'none', md:'block'}} isLoading={isLoggingOut} variant={"ghost"} _hover={{bg:"transparent"}}>Log out</Button>
                 </Flex>
